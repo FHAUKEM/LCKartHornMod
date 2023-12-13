8c48cffd942ab15b6f47a03e44869b4ebf4adb5b
fabian <f.h.aukema@student.rug.nl>
2023-12-13 21:25:10 +0100

    First release

---
+++ LCKartHornMod/HornMaster.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using BepInEx;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace LCKartHornMod;
+
+public class HornMaster
+{
+    public static List<AudioClip> Horns = [];
+    public static List<AudioClip> HornsFar = [];
+
+    public static void LoadHorns()
+    {
+        string path = Path.Combine(Paths.BepInExRootPath, "plugins", "Horns");
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+        string[] files = Directory.GetFiles(path, "*.ogg");
+        
+        if (files.Length == 0)
+        {
+            Plugin.HornLog.LogInfo("No horns found!");
+            return;
+        }
+        
+        Plugin.HornLog.LogInfo($"Attempting to load {files.Length} horns...");
+        foreach (string file in files)
+        {
+            AudioClip clip = LoadAudioFile(file);
+            if (clip != null)
+            {
+                Horns.Add(clip);
+                HornsFar.Add(LowerVolumeByHalf(clip));
+            }
+        }
+        // Sort the horns by name
+        Horns.Sort((a, b) => string.Compare(a.name, b.name, StringComparison.Ordinal));
+        HornsFar.Sort((a, b) => string.Compare(a.name, b.name, StringComparison.Ordinal));
+    }
+    
+    private static AudioClip LoadAudioFile(string path)
+    {
+        UnityWebRequest loader = UnityWebRequestMultimedia.GetAudioClip(path, AudioType.OGGVORBIS);
+        loader.SendWebRequest();
+        while (!loader.isDone)
+        {
+            // Wait for the file to load
+        }
+        if (loader.error != null)
+        {
+            Plugin.HornLog.LogError($"Failed to load audio file {path}: {loader.error}");
+            return null;
+        }
+        AudioClip clip = DownloadHandlerAudioClip.GetContent(loader);
+        clip.name = Path.GetFileNameWithoutExtension(path);
+        return clip;
+    }
+    
+    private static AudioClip LowerVolumeByHalf(AudioClip originalClip)
+    {
+        // This should not decrease the original clip's volume
+        AudioClip clip = AudioClip.Create(originalClip.name, originalClip.samples, originalClip.channels, originalClip.frequency, false);
+        float[] samples = new float[originalClip.samples * originalClip.channels];
+        originalClip.GetData(samples, 0);
+        for (int i = 0; i < samples.Length; i++)
+        {
+            samples[i] *= 0.05f;
+        }
+        clip.SetData(samples, 0);
+        return clip;
+    }
+}
---
+++ LCKartHornMod/Plugin.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LCKartHornMod
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Lethal Company.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Plugin Instance;
+        public static ManualLogSource HornLog => Instance.Logger;
+        
+        // List of keyvalue pairs of scrap value and how many times it appears
+        public static Dictionary<int, int> ScrapValues = new Dictionary<int, int>();
+
+        private void Awake()
+        {
+            Instance ??= this;
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loading!");
+
+            // Load audio files
+            HornMaster.LoadHorns();
+            
+            // Patch the game
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+            
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} loaded succesfully!");
+        }
+
+        [HarmonyPatch(typeof(GrabbableObject), "SetScrapValue")]
+        [HarmonyPostfix]
+        private static void AudioPatch(GrabbableObject __instance)
+        {
+            if ((Object)__instance != (Object)null &&
+                (Object)((Component)__instance).GetComponent<NoisemakerProp>() != (Object)null &&
+                ((Object)__instance.itemProperties).name == "ClownHorn")
+            {
+                HornLog.LogInfo("Found clown horn! Replacing audio clip...");
+                
+                // Store the scrap value in a dictionary
+                if (ScrapValues.ContainsKey(__instance.scrapValue))
+                {
+                    ScrapValues[__instance.scrapValue]++;
+                }
+                else
+                {
+                    ScrapValues.Add(__instance.scrapValue, 1);
+                }
+                
+                // Create a random index based on System.Random(scrap value + scrap value count)
+                int index = new System.Random(__instance.scrapValue + ScrapValues[__instance.scrapValue]).Next(0,
+                    HornMaster.Horns.Count);
+                
+                HornLog.LogInfo("Replaced with " + HornMaster.Horns[index].name + "!");
+                
+                ((Component)__instance).GetComponent<NoisemakerProp>().noiseSFX[0] = HornMaster.Horns[index];
+                ((Component)__instance).GetComponent<NoisemakerProp>().noiseSFXFar[0] = HornMaster.HornsFar[index];
+            }
+        }
+    }
+}
